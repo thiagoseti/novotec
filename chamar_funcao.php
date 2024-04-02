9339191f11fcde28c987f8066becfa86f25e71d7
thiagoseti <83315376+thiagoseti@users.noreply.github.com>
2024-04-02 16:25:15 -0300

    Update chamar_funcao.php

--- chamar_funcao.php
+++ chamar_funcao.php
@@ -2,6 +2,6 @@
     include 'funcao_separada.php'; //Incluindo o funcao_separada.php
 
     verificarIdade(1986); //1ª chamada
-    verificarIdade(2016); //2ª chamada
+    verificarIdade(2016); //2ª chamada da função
 
 ?>
