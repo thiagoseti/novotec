0363ac74009acd0a47896b49754dfcd0b180002d
Thiago <thiago-2-pc@hotmail.com>
2024-04-02 16:30:08 -0300

    Commit 2

--- chamar_funcao.php
+++ chamar_funcao.php
@@ -2,6 +2,7 @@
     include 'funcao_separada.php'; //Incluindo o funcao_separada.php
 
     verificarIdade(1986); //1ª chamada
+    echo "<br>";
     verificarIdade(2016); //2ª chamada da função
 
 ?>
